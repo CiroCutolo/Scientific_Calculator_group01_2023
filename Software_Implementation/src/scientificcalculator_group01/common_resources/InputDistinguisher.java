cdfa57a396fac405536bfd22576bf685b57e3acf
CiroCutolo <cirocutolo2002@gmail.com>
2023-12-06 17:56:59 +0100

    modifica all'InputDistinguisher. Cutolo Ciro

--- Software_Implementation/src/scientificcalculator_group01/common_resources/InputDistinguisher.java
+++ Software_Implementation/src/scientificcalculator_group01/common_resources/InputDistinguisher.java
@@ -15,19 +15,28 @@ public class InputDistinguisher {
     }
     
     public boolean isComplexNumber(String text){
-        
+        // '*' impone l'apparizione del testo che lo precede nella matches 0 o più volte.
+        // '?' esplicita che il testo da cui è preceduto nella matches non debba per forza essere presente nella stringa od al massimo una singola volta.
+        if(text.matches("^[+-]?[0-9]+[.]?[0-9]*$")){
+            return true;
+        }else if(text.matches("^[+-]([0-9]+[.]?[0-9]*)?[ij]$")){
+            return true;
+        }else if(text.matches("^[+-]?[0-9]+[.]?[0-9]*[+-]([0-9]+[.]?[0-9]*)?[ij]$")){
+            return true;
+        }
+        return false;
     }
     
     public boolean isStackOperation(String text){
-        
+        return false;
     }
     
     public boolean isMathOperation(String text){
-        
+        return false;
     }
     
     public boolean isVariablesOperation(String text){
-        
+        return false;
     }
 }
 
--- Software_Implementation/src/scientificcalculator_group01/variablesoperations/SaveIntoVarOperation.java
+++ Software_Implementation/src/scientificcalculator_group01/variablesoperations/SaveIntoVarOperation.java
@@ -5,10 +5,32 @@
  */
 package scientificcalculator_group01.variablesoperations;
 
+import java.util.Stack;
+
+import scientificcalculator_group01.common_resources.ComplexNumber;
+import scientificcalculator_group01.common_resources.Variables;
+import scientificcalculator_group01.exceptions.StackErrorException;
+import scientificcalculator_group01.exceptions.SyntaxErrorException;
+
 /**
  *
  * @author ciroc
  */
-public class SaveIntoVarOperation {
-    
+public class SaveIntoVarOperation extends VariablesOperation{
+    public SaveIntoVarOperation(){
+        super(1);
+    }
+
+    public void execute(Variables variable, Stack<ComplexNumber> stack, char name) throws SyntaxErrorException, StackErrorException{
+        if(!super.minOperandsToOperate(stack.size())){
+            throw new StackErrorException("Not enough operands given...");
+        }
+        ComplexNumber num = stack.peek();
+        if(num.getA() == 0 && num.getB() == 0){
+            throw new SyntaxErrorException("The operands were wrongly typed...");
+        }else{
+            variable.setVariable(name, num);
+        }
+
+    }
 }
