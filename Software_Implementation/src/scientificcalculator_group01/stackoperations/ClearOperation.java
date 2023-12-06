5562a6bddc6de21c9180f444669f1139f61e7cb6
Gaetano Frasca <gaetanofra02@gmail.com>
2023-12-06 22:43:35 +0100

    Merge origin/main into main
    Conflicts:
    	Software_Implementation/src/scientificcalculator_group01/mathoperations/MultiplicationOperation.java

--- Software_Implementation/src/scientificcalculator_group01/stackoperations/ClearOperation.java
+++ Software_Implementation/src/scientificcalculator_group01/stackoperations/ClearOperation.java
@@ -5,10 +5,38 @@
  */
 package scientificcalculator_group01.stackoperations;
 
+import java.util.Stack;
+import scientificcalculator_group01.common_resources.ComplexNumber;
+
 /**
  *
- * @author ciroc
+ * @author Gaetano Frasca
+ * Questa classe fornisce un metodo per eseguire il comando "clear", che
+ * consente di eliminare tutti gli elementi presenti nello stack.
  */
-public class ClearOperation {
+public class ClearOperation extends StackOperation{
     
+    /**
+     * @descrizione Costruttore di default
+     *              Richiama il costruttore della superclasse StackOperation, 
+     *              fornendo come parametro il numero di operandi necessari 
+     *              affinchè il comando "clear" possa essere eseguito 
+     *              correttamente, in questo caso viene specificato come
+     *              parametro 0, perchè l'operazione rimuove tutti gli elementi
+     *              presenti nellos tack.
+     */
+    public ClearOperation(){
+        super(0);
+    }
+    
+    /**
+     * @descrizione Esegue il comando "clear", il metodo non fa altro che
+     *              richiamare il metodo già presente nella libreria java.util.Stack.
+     * @param stack 
+     */
+    @Override
+    public void execute(Stack<ComplexNumber> stack){
+        
+        stack.clear();
+    }
 }
