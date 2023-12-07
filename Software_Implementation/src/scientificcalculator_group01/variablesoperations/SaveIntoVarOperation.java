79de61c92c2d0f75702d506b22a3b3fbbd2f22d2
CiroCutolo <cirocutolo2002@gmail.com>
2023-12-07 13:43:44 +0100

    Modifiche alla classe SaveIntoVarOperation Cutolo Ciro

--- Software_Implementation/src/scientificcalculator_group01/variablesoperations/SaveIntoVarOperation.java
+++ Software_Implementation/src/scientificcalculator_group01/variablesoperations/SaveIntoVarOperation.java
@@ -24,11 +24,10 @@ public class SaveIntoVarOperation extends VariablesOperation{
     public void execute(Variables variable, Stack<ComplexNumber> stack, char name) throws SyntaxErrorException, StackErrorException{
         if(!super.minOperandsToOperate(stack.size())){
             throw new StackErrorException("Not enough operands given...");
-        }
-        ComplexNumber num = stack.peek();
-        if(num.getA() == 0 && num.getB() == 0){
-            throw new SyntaxErrorException("The operands were wrongly typed...");
         }else{
+         
+            ComplexNumber num = stack.peek();
+
             variable.setVariable(name, num);
         }
 
